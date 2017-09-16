5f1bd89aa75a23df18e143c74010442ea0ab48e4
Diego Antonino <diego_antonino@hotmail.com>
2017-09-16 10:49:13 -0400

    first draft

---
+++ control_fan.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import os
+from time import sleep
+import RPi.GPIO as GPIO
+
+pin = 18 # The pin ID, edit here to change it
+maxTMP = 40 # The maximum temperature in Celsius after which we trigger the fan
+
+def main():
+    try:
+        setup()
+    while True:
+        controlFan()
+        sleep(5) # Read the temperature every 5 sec, increase or decrease this limit if you want
+    except Exception: # trap a CTRL+C keyboard interrupt
+        GPIO.cleanup() # resets all GPIO ports used by this program
+
+
+def setup():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.setwarnings(False)
+    return()
+
+
+def getCPUtemperature():
+    temp = float(os.popen('vcgencmd measure_temp').readline().replace("temp=", "").replace("'C\n", ""))
+    return temp
+
+
+def controlFan():
+    CPU_temp = getCPUtemperature()
+    if CPU_temp > maxTMP and not GPIO.output(pin):
+        GPIO.output(pin, True)
+    elif CPU_temp < (maxTMP-0.1*maxTMP) and GPIO.output(pin):
+        GPIO.output(pin, False)
+    return()
+
+
+def setPin(mode): # A little redundant function but useful if you want to add logging
+    GPIO.output(pin, mode)
+    return()
+
+if __name__ == "__main__":
+    main()
